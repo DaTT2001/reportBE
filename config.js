e725a7cdc5526bd3afe60b84b6568a5a47d9b51d
DaTT2001 <congdatylvt@gmail.com>
2025-06-19 11:06:00 +0700

    update google

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   CAMERA_IP: '192.168.24.136',
   USERNAME: 'admin',
   PASSWORD: 'kinhduan12@',
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbxDnL8x5s6a3CqCuQba_rbgJqvxGoD3z3mg60K2CDXfDeShjq7UjRq2u55kVPF8xmjP/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbx1FUmtIQWREk8eiO8E62hdi9n02gTvenZTD-Yi457HzpCHBF5XblMPuYm8ABXssgJM/exec',
 };
