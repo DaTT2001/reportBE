4c8e3f90c9b51c5a4b7909c6b208d412baf1b1b4
Duong Cong Dat <104434271+DaTT2001@users.noreply.github.com>
2025-06-24 07:48:38 +0700

    okma

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     CAMERA_IP: '192.168.24.136',
     USERNAME: 'admin',
     PASSWORD: 'kinhduan12@',
-    GAS_URL: 'https://script.google.com/macros/s/AKfycbx1FUmtIQWREk8eiO8E62hdi9n02gTvenZTD-Yi457HzpCHBF5XblMPuYm8ABXssgJM/exec',
+    GAS_URL: 'https://script.google.com/macros/s/AKfycby0iGrTqDpt7FiCUlIAtwRZ9Jiz8_lEGtWAWb25j2CwTNRoEjHZWQ5G1KPxdB5wd0cC/exec',
     GAS_URL2: 'https://script.google.com/macros/s/AKfycbwjTjQR5sqa3y8lK9oatGP30HjfShQO82mxk7koE-icN8oHHBbAUD0l-npAdMwR_lm0yQ/exec',
 };
