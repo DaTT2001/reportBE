30f4911965f8c3bb93a05d0df40deadd5dbbfc69
Duong Cong Dat <104434271+DaTT2001@users.noreply.github.com>
2025-06-26 08:59:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     CAMERA_IP: '192.168.24.136',
     USERNAME: 'admin',
     PASSWORD: 'kinhduan12@',
-    GAS_URL: 'https://script.google.com/macros/s/AKfycby0iGrTqDpt7FiCUlIAtwRZ9Jiz8_lEGtWAWb25j2CwTNRoEjHZWQ5G1KPxdB5wd0cC/exec',
+    GAS_URL: 'https://script.google.com/macros/s/AKfycbzvQ62LPctCVwzwZfX90NbBMZtACWpQiH3ZZWOzkr_4NNPpMPJwsmxP4fty27_ftsxh/exec',
     GAS_URL2: 'https://script.google.com/macros/s/AKfycbwjTjQR5sqa3y8lK9oatGP30HjfShQO82mxk7koE-icN8oHHBbAUD0l-npAdMwR_lm0yQ/exec',
 };
